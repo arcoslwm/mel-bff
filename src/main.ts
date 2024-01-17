d059384c42529429a6a918880709cb8b7df4fad1
Arcos.lwm <arcos.lwm@gmail.com>
2024-01-17 01:46:34 -0300

    agrega swagger

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,33 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger, LoggerErrorInterceptor } from 'nestjs-pino';
+import {
+    DocumentBuilder,
+    SwaggerDocumentOptions,
+    SwaggerModule,
+} from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {bufferLogs:true});
-  app.useLogger(app.get(Logger));
-  app.useGlobalInterceptors(new LoggerErrorInterceptor());
-  await app.listen(3001);
+    const app = await NestFactory.create(AppModule, { bufferLogs: true });
+
+    app.useLogger(app.get(Logger));
+    app.useGlobalInterceptors(new LoggerErrorInterceptor());
+
+    const config = new DocumentBuilder()
+        .setTitle('API BFF MeLi')
+        .setDescription('Búsqueda y detalle de productos (items) MeLi')
+        .setVersion('1.0')
+        .addTag('MeLi')
+        .build();
+
+    const options: SwaggerDocumentOptions = {
+        operationIdFactory: (controllerKey: string, methodKey: string) =>
+            methodKey,
+    };
+
+    const document = SwaggerModule.createDocument(app, config, options);
+    SwaggerModule.setup('api/docs', app, document);
+
+    await app.listen(3001);
 }
 bootstrap();
